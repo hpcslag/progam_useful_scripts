ef98d2b378f604b1530041754f1318610ee8de88
MacTaylor <hpcslag@users.noreply.github.com>
2021-07-09 22:59:52 +0900

    Update create_slido_by_sessions.py

--- create_slido_by_sessions.py
+++ create_slido_by_sessions.py
@@ -5,11 +5,16 @@ import iso8601
 import pytz
 import time
 
-slido_account = ""  # 請填上 sli.do account id
+slido_account = "" # 請填上 sli.do account id, 在 F12 Network 隨便開一個請求應該可查到 query string 有 account_id=?
 slido_authorization = "" # 請填上 sli.do Bearer Token Code (不用複製 `Bearer`，僅複製他後面的那串 token)
-create_period = 3.5 #sec
 
-sessions = json.load(open("./schedule.json", ))  # 須從 pretalx 上下載好 schedule.json
+# 防止 429 too many requests 最佳配置
+create_period = 3.5 #sec
+rate_halt_period = 60 #sec
+
+# 會讀取跟本腳本同目錄下的 scheudle.json
+# https://pretalx.com/[Your Events]/schedule/v/wip/ can download the json schedule table
+sessions = json.load(open("./schedule.json", ))
 
 def parse_date(date_str, spec_hour=9):
   _date_obj = iso8601.parse_date(date_str)
@@ -47,11 +52,16 @@ def create_slido_room_by_session_name(
 
   response = requests.request("POST", url, headers=headers, data=payload)
 
-  #print(response.text)
+  print(response.text)
   slido_response = json.loads(response.text)
   return slido_response["url"]["app"]
 
+
 if __name__ == '__main__':
+  # halt every 5 sec (429 too many requests)
+  _rate_limit_count = 0
+
+
   export_file = open("pretalx_session_slido.json", "w")
   export_data = []
 
@@ -65,6 +75,10 @@ if __name__ == '__main__':
     for room_name in day["rooms"].keys():
       print("-------- " + room_name + " ----------")
       for idx, room in enumerate(day["rooms"][room_name]):
+        # add rate count
+        _rate_limit_count += 1
+
+        # add session to slido
         session_id = room["id"]
         session_title = room["title"]
         session_code = conference_acronym+"-"+str(session_id)
@@ -77,12 +91,17 @@ if __name__ == '__main__':
         )
         time.sleep(create_period)
         
+        # add data to export array
         export_data.append({
           "session_id": session_id,
           "session_title": session_title,
           "slido_session_code": session_code,
           "slido_url": slido_url
         })
+
+        # halt count (slido is limited every 5 slido in minutes)
+        if _rate_limit_count > 1 and _rate_limit_count % 5 == 0:
+          time.sleep(rate_halt_period)
 
   json_dump = json.dumps(export_data)
   export_file.write(json_dump)
